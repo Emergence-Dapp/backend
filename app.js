86e9240418a5dd8e18945ae9d7108e892190631e
Shayan S <91401566+sha256yan@users.noreply.github.com>
2022-08-21 00:54:51 -0500

    changed origin to allow all

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors({
-    origin: "https://emergence-gamma.vercel.app/"
+    origin: "*"
 }))
 
 const port = process.env.PORT || 5001;

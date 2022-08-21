aef07b065c81d54e8b347487cad50bbf22896042
Shayan S <91401566+sha256yan@users.noreply.github.com>
2022-08-21 02:22:32 -0500

    added Meetings, Meeting, Rating classes

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ const express = require('express');
 const { ethers } = require('ethers');
 const cors = require('cors');
 
-
-
+const { Meetings, Meeting, Rating } = require('./classes/Meetings');
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors({
     origin: "*"
-}))
+}));
+
+
 
 const port = process.env.PORT || 5001;
+const meetings = new Meetings();
+
+
 
 
 
@@ -26,6 +29,45 @@ app.post('/', (req, res) => {
     res.send('{"bruh": "1"}');
 })
 
+
+app.post('/submit-rating', (req, res) => {
+
+  const rating = new Rating(req.body);
+  //add rating to appropriate meeting
+
+  res.send('{"bruh": "1"}');
+
+})
+
+
+
+app.post('/open-meeting', (req, res) => {
+
+  //instantiate a new Meeting in Meetings.
+
+  const meetingId = req.body.id;
+  
+  meetings.addMeeting(meetingId);
+
+})
+
+
+
+app.post('/close-meeting', (req, res) => {
+  
+  const meetingId = req.body.id;
+
+  meetings.removeMeeting(meetingId);
+
+})
+
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+
+
---
+++ classes/Meetings.js
@@ -0,0 +1,103 @@
+/*
+  Holds all open meetings, and manages closing meetings.
+
+  NOTE: on-chain closing call should be placed in the Meeting.closeMeeting funciton.
+*/
+
+
+class Meetings {
+
+    constructor(){
+      this.meetings = {};
+    }
+  
+    addMeeting(meetingId) {
+      const newMeeting = new Meeting();
+  
+      if(this.meetings[meetingId] !== undefined) return;
+  
+      this.meetings[meetingId] = newMeeting;
+    }
+  
+    removeMeeting(meetingId) {
+      const meetingToClose = this.meetings[meetingId];
+      if(meetingToClose == undefined)return;
+  
+  
+      meetingToClose.closeMeeting(); //do neccessary closing ops.
+  
+      this.meetings[meetingId] = undefined; //set meeting to undefined
+    }  
+}
+  
+  
+  
+  
+  
+  
+  
+class Meeting {
+
+    constructor() {
+        this.ratings = []; //will be an array of Rating objects.
+        this.isOpen = true;
+    }
+
+
+    addRating(ratingData) {
+        if(!this.isOpen)return;
+
+        const rating = new Rating(ratingData);
+        this.ratings.push(rating);
+    }
+
+
+
+    closeMeeting() {
+        this.isOpen = false;
+        const scores = this.aggregateRatingScores();
+
+        //whatever is needed when a meeting closes.
+    }
+
+
+
+    aggregateRatingScores() {
+        const scores = this.ratings.map(rating => rating.rating);
+        const numRatings = scores.length;
+        const meanRating = scores.reduce((score1, score2) => score1 + score2, 0) / numRatings;
+
+        return { numRatings, meanRating };
+    }
+
+}
+
+
+
+
+
+
+
+
+
+class Rating {
+    constructor(ratingData) {
+        this.id = ratingData.id;
+        this.name = ratingData.name;
+        this.rating = ratingData.rating;
+        this.room = ratingData.room;
+        this.purpose = ratingData.purpose;
+        this.secondaryPurpose = ratingData.secondaryPurpose;
+        this.paid = ratingData.paid;
+        this.denomiation = ratingData.denomiation;
+        this.date = ratingData.denomiation;
+        this.duration = ratingData.duration;
+    }
+}
+
+
+module.exports = {
+    Meetings,
+    Rating,
+    Meeting
+}
